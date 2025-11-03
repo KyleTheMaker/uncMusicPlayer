c2b489b0b71fbaaf232c509ed58724460dfc25e5
KyleTheMaker <kyle.wj.maddox@gmail.com>
2025-11-03 13:44:26 -0800

    brought in the MediaPlayer design update and fixed merge conflicts and bugs

--- App.js
+++ App.js
@@ -24,6 +24,7 @@ import { Ionicons } from "@expo/vector-icons";
 import { SongProvider } from "./context/SongContext";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { SQLiteProvider, useSQLiteContext } from "expo-sqlite";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import { manageDBIfNeeded } from "./data/musicdb";
 
@@ -39,37 +40,41 @@ export default function App() {
     <SafeAreaProvider>
       <SQLiteProvider databaseName="uncMusic.db" onInit={manageDBIfNeeded}>
         <SongProvider>
-          <NavigationContainer>
-            <Tab.Navigator
-              screenOptions={({ route }) => ({
-                tabBarIcon: ({ focused, color, size }) => {
-                  let iconName;
+          <GestureHandlerRootView>
+            <NavigationContainer>
+              <Tab.Navigator
+                screenOptions={({ route }) => ({
+                  tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
 
-                  if (route.name === "Home") {
-                    iconName = focused ? "home" : "home-outline";
-                  } else if (route.name === "Playlist") {
-                    iconName = focused ? "albums" : "albums-outline";
-                  } else if (route.name === "Music Player") {
-                    iconName = focused
-                      ? "musical-notes"
-                      : "musical-notes-outline";
-                  } else if (route.name === "Settings") {
-                    iconName = focused ? "settings" : "settings-outline";
-                  }
+                    if (route.name === "Home") {
+                      iconName = focused ? "home" : "home-outline";
+                    } else if (route.name === "Playlist") {
+                      iconName = focused ? "albums" : "albums-outline";
+                    } else if (route.name === "Music Player") {
+                      iconName = focused
+                        ? "musical-notes"
+                        : "musical-notes-outline";
+                    } else if (route.name === "Settings") {
+                      iconName = focused ? "settings" : "settings-outline";
+                    }
 
-                  // You can return any component that you like here!
-                  return <Ionicons name={iconName} size={size} color={color} />;
-                },
-                tabBarActiveTintColor: "blue",
-                tabBarInactiveTintColor: "gray",
-              })}
-            >
-              <Tab.Screen name="Home" component={HomeScreen} />
-              <Tab.Screen name="Playlist" component={PlaylistScreen} />
-              <Tab.Screen name="Music Player" component={MediaPlayerScreen} />
-              <Tab.Screen name="Settings" component={SettingsScreen} />
-            </Tab.Navigator>
-          </NavigationContainer>
+                    // You can return any component that you like here!
+                    return (
+                      <Ionicons name={iconName} size={size} color={color} />
+                    );
+                  },
+                  tabBarActiveTintColor: "blue",
+                  tabBarInactiveTintColor: "gray",
+                })}
+              >
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="Playlist" component={PlaylistScreen} />
+                <Tab.Screen name="Music Player" component={MediaPlayerScreen} />
+                <Tab.Screen name="Settings" component={SettingsScreen} />
+              </Tab.Navigator>
+            </NavigationContainer>
+          </GestureHandlerRootView>
         </SongProvider>
       </SQLiteProvider>
     </SafeAreaProvider>
--- components/MediaPlayer.js
+++ components/MediaPlayer.js
@@ -12,10 +12,20 @@
  *
  *
  */
-import { StyleSheet, Text, View, Button, Pressable, Alert } from "react-native";
-import { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Pressable,
+  Alert,
+  Image,
+} from "react-native";
+import { useState, useEffect } from "react";
 import { useAudioPlayer, useAudioPlayerStatus } from "expo-audio";
 import Slider from "@react-native-community/slider";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+
 import MediaButton from "./MediaButton";
 import { SongContext, useSongPlayer } from "../context/SongContext";
 
@@ -24,13 +34,51 @@ const MediaPlayer = () => {
 
   const [songPosition, setSongPosition] = useState(0);
   const player = useAudioPlayer(isLoading ? null : currentSong.location);
+  const [isPlay, setIsPlay] = useState(true);
+  const [currentTime, setCurrentTime] = useState("");
+  const [formattedSongDuration, setFormattedSongDuration] = useState("0:00");
   const status = useAudioPlayerStatus(player)?.currentTime || 0;
   const duration = player?.duration || 0;
+  const coverImages = [
+    require("../assets/vinyl-record.gif"),
+    require("../assets/vinyl-record-static.png"),
+  ];
+  const [currentImage, setCurrentImage] = useState(coverImages[0]);
+  const MAX_TRANSLATION_Y = 300;
+
+  // update current time
+  useEffect(() => {
+    setCurrentTime(formatTime(status));
+  }, [status]);
+
+  // update song duration
+  useEffect(() => {
+    if (duration == 0) return;
+    else setFormattedSongDuration(formatTime(duration));
+  }, [duration]);
+
+  // play on song change
+  useEffect(() => {
+    if (!isLoading && currentSong.location && player.isLoaded) {
+      player.play();
+      setIsPlay(true);
+    }
+  }, [currentSong.location, player.isLoaded, isLoading]);
+
+  // change image when isPlay variable changed
+  useEffect(() => {
+    if (isPlay) {
+      setCurrentImage(coverImages[0]);
+    } else {
+      setCurrentImage(coverImages[1]);
+    }
+  }, [isPlay]);
+
   const formatTime = (totalSeconds) => {
-        const minutes = Math.floor(totalSeconds / 60);
-        const seconds = Math.floor(totalSeconds % 60);
-        return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-    };
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = Math.floor(totalSeconds % 60);
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  };
   const timeDisplay = formatTime(duration);
   const statusDisplay = formatTime(status);
 
@@ -42,72 +90,145 @@ const MediaPlayer = () => {
     );
   }
 
+  const handlePlayButton = () => {
+    if (isPlay) {
+      player.pause();
+    } else {
+      player.play();
+    }
+
+    setIsPlay((prevVal) => !prevVal);
+  };
+
   useEffect(() => {
     if (!isLoading && currentSong.location && player.isLoaded) {
       player.play();
     }
   }, [currentSong.location, player.isLoaded, isLoading]);
 
+  useEffect(() => {
+    console.log(`volume:${player.volume}`);
+  }, [player.volume]);
+
+  const panGesture = Gesture.Pan().onEnd((e) => {
+    const { translationX, translationY } = e;
+
+    if (Math.abs(translationX) > Math.abs(translationY)) {
+      // Horizontal swipe
+      if (translationX > 50) {
+        console.log("Swiped right");
+      } else if (translationX < -50) {
+        console.log("Swiped left");
+      }
+      console.log(translationX);
+    } else {
+      // Vertical swipe
+      let normalizedY = Math.min(Math.abs(translationY) / MAX_TRANSLATION_Y, 1); // normalized value between 0-1
+
+      if (translationY > 20) {
+        // swiped down, decrease volume
+        normalizedY *= -1;
+        console.log("swiped down");
+      } else if (translationY < 20) {
+        // swiped up, increase volume
+        console.log("swiped up");
+      }
+
+      let newVolume = player.volume + normalizedY;
+
+      if (newVolume >= 1) newVolume = 1;
+      else if (newVolume <= 0) newVolume = 0.0;
+
+      player.volume = newVolume;
+    }
+  });
+
   return (
-    <View style={styles.mediaPlayer}>
-      <Text>Now Playing: {currentSong.name}</Text>
-      <Text>Song Length: {timeDisplay}</Text>
-      <View style={styles.buttonContainer}>
-        <MediaButton text="Previous" pressOut={() => changeTrack(-1)} />
-        <MediaButton text="Play" pressOut={() => player.play()} />
-        <MediaButton text="Pause" pressOut={() => player.pause()} />
-        <MediaButton text="Next" pressOut={() => changeTrack(1)} />
+    <GestureDetector gesture={panGesture}>
+      <View style={styles.mediaPlayer}>
+        <View style={styles.imageContainer}>
+          <Image style={styles.coverImage} source={currentImage} />
+        </View>
+
+        <View style={styles.musicBarContainer}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              width: "100%",
+            }}
+          >
+            <Text>{currentTime}</Text>
+            <Text>{formattedSongDuration}</Text>
+          </View>
+          <Slider
+            style={{ width: "100%", margin: 5 }}
+            minimumValue={0}
+            maximumValue={duration}
+            step={1}
+            value={status}
+            onSlidingComplete={(value) => player.seekTo(value)}
+            minimumTrackTintColor="#2e3299ff"
+            maximumTrackTintColor="#000000"
+          />
+          <Text style={{ fontSize: 30, textAlign: "center", margin: 5 }}>
+            {currentSong.name}
+          </Text>
+          {/* <Text style={{fontSize: 14, textAlign: 'center', margin: 5}}>Track #{currentSong.name}</Text> */}
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <View style={styles.buttonRowContainer}>
+            <MediaButton
+              icon="play-skip-back"
+              size={60}
+              pressOut={() => changeTrack(-1)}
+            />
+            <MediaButton
+              icon={isPlay ? "pause-circle" : "play-circle"}
+              size={90}
+              pressOut={handlePlayButton}
+            />
+            <MediaButton
+              icon="play-skip-forward"
+              size={60}
+              pressOut={() => changeTrack(1)}
+            />
+          </View>
+        </View>
       </View>
-      <View style={styles.buttonContainer}>
-        <MediaButton text="Restart" pressOut={() => player.seekTo(0)} />
-        <MediaButton
-          text="Press You!"
-          pressOut={() => {
-            Alert.alert("Don't Panic!");
-          }}
-        />
-      </View>
-      <View style={{ flexDirection: "row", justifyContent: "center" }}>
-        <Slider
-          style={{ width: 300, height: 20, margin: 5 }}
-          minimumValue={0}
-          maximumValue={duration}
-          step={1}
-          value={status}
-          onSlidingComplete={(value) => player.seekTo(value)}
-          minimumTrackTintColor="#2e3299ff"
-          maximumTrackTintColor="#000000"
-        />
-        <Text>
-          {(status / 60).toFixed(0) +
-            ":" +
-            (status < 10 ? "0" : "") +
-            (status % 60).toFixed(0)}
-        </Text>
-      </View>
-    </View>
+    </GestureDetector>
   );
 };
 
 const styles = StyleSheet.create({
   mediaPlayer: {
-    marginBottom: 5,
+    flex: 1,
+    padding: 8,
   },
-  buttonContainer: {
-    margin: 5,
-    flexDirection: "row",
-    flexWrap: "wrap",
+  imageContainer: {
+    flex: 3,
+    alignItems: "center",
     justifyContent: "center",
   },
-  pressBtn: {
-    backgroundColor: "#ffa",
-    fontWeight: "heavy",
-    margin: 5,
-    borderWidth: 2,
-    borderRadius: 20,
-    padding: 10,
-    height: "auto",
-    width: "auto",
+  coverImage: {
+    width: 350,
+    height: 350,
+    resizeMode: "cover",
+  },
+  musicBarContainer: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  buttonRowContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
