e867da812c8b3f506684f156ae46e3ed58d7b9e8
KyleTheMaker <kyle.wj.maddox@gmail.com>
2025-11-21 12:13:39 -0800

    removed expo media library and added folder selection with song listing. file copying for playback still needs to be implemented

--- components/FileSelector.js
+++ components/FileSelector.js
@@ -9,7 +9,10 @@ import {
 import { useState, useEffect } from "react";
 import * as DocumentPicker from "expo-document-picker";
 import { Directory, Paths, File } from "expo-file-system";
-import * as MediaLibrary from "expo-media-library";
+import { useSongPlayer } from "../context/SongContext";
+
+import Song from "./Song";
+// import * as MediaLibrary from "expo-media-library";
 
 /**
  * Exploring expo-media-library
@@ -19,34 +22,25 @@ import * as MediaLibrary from "expo-media-library";
 
 const FileSelector = () => {
   const [chosenFolder, setChosenFolder] = useState("No File Chosen");
-  const [albums, setAlbums] = useState(null);
-  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
-
-  async function getAlbums() {
-    if (permissionResponse.status !== "granted") {
-      await requestPermission();
-    }
-    const fetchedAlbums = await MediaLibrary.getAlbumsAsync({
-      includeSmartAlbums: true,
-    });
-    setAlbums(fetchedAlbums);
-    console.log(fetchedAlbums.filter((album)=>{return album.title == "Music"}));
-  }
+  const [localSongs, setLocalSongs] = useState([]);
+  const [songsList, setSongsList] = useState([]);
+  const { playNewSong } = useSongPlayer();
 
   //look for Audio files folder on Phone
   const chooseFolder = async () => {
     try {
-      const chosenDoc = await DocumentPicker.getDocumentAsync({
-        copyToCacheDirectory: true,
+      const directory = await new Directory.pickDirectoryAsync();
+      const directoryItems = directory.list().filter((item) => {
+        return item.type == "audio/mpeg";
       });
-      const folder = new File(chosenDoc.assets[0]);
-      console.log("File URI: ", folder.uri);
-      const fileName = chosenDoc.assets[0].name;
-      console.log("awaited File Name: ", fileName);
-      setChosenFolder(fileName);
+      setLocalSongs(directoryItems);
+      console.log(directoryItems);
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
+  };
+  const handlePlaySong = (location, name, listArray, listIndex) => {
+    playNewSong(location, name, listArray, listIndex);
   };
 
   return (
@@ -55,47 +49,24 @@ const FileSelector = () => {
         <Text>This is the FileSelector Component</Text>
         <Button title="Choose File" onPress={chooseFolder} />
         <Text>Chosen File: {chosenFolder}</Text>
-      </View>
-      <View style={styles.section}>
-        <Button onPress={getAlbums} title="Get albums" />
         <ScrollView>
-          {albums &&
-            albums.map((album) => <AlbumEntry album={album} key={album.id} />)}
+          {localSongs &&
+            localSongs.map((song, index) => (
+              <Song
+                songName={song.name}
+                actionText={"Remove Song"}
+                songLocation={song.uri}
+                playSong={(location, name) =>
+                  handlePlaySong(location, name, songsList, index)
+                }
+                key={song.md5}
+              />
+            ))}
         </ScrollView>
       </View>
     </View>
   );
 };
-
-function AlbumEntry({ album }) {
-  const [assets, setAssets] = useState([]);
-
-  useEffect(() => {
-    async function getAlbumAssets() {
-      const albumAssets = await MediaLibrary.getAssetsAsync({ album });
-      setAssets(albumAssets.assets);
-    }
-    getAlbumAssets();
-    console.log("Assets:", {assets});
-  }, [album]);
-
-  return (
-    <View key={album.id} style={styles.section}>
-      <Text>
-        {album.title} - {album.assetCount ?? "no"} assets
-      </Text>
-      <View style={styles.container}>
-        {assets &&
-          assets.map((asset) => (
-            <View key={asset.id}>
-              <Image source={{ uri: asset.uri }} width={50} height={50} />
-              <Text>{asset.uri}</Text>
-            </View>
-          ))}
-      </View>
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
